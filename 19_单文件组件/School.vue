d869f2304a921c73f24c330a43835ff7bb446cda
zhujiayi <1352480619@qq.com>
2022-06-20 01:03:04 +0800

    Initial commit

---
+++ 19_单文件组件/School.vue
@@ -0,0 +1,33 @@
+<!--组件的结构（页面html模版）-->
+<template>
+    <div id="demo">
+      <h2>学校名称：{{schoolName}}</h2>
+      <h2>学校地址：{{schoolAddress}}</h2>
+      <button @click="showShowName">点击显示学校名称</button>
+    </div>
+</template>
+
+<!--组件的交互（js数据交互）-->
+<script>
+    export default {
+      name:'school',
+      data(){
+        return{
+          schoolName:'清华大学',
+          schoolAddress:'北京'
+        }
+      },
+      methods:{
+        showShowName(){
+          alert(this.schoolName)
+        }
+      }
+    }
+</script>
+
+<!--组件的样式（css样式）-->
+<style>
+  #demo{
+    background-color: aquamarine;
+  }
+</style>
